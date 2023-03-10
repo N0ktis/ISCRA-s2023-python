9f58dcd0b4cabb4ab4f4d2f32687431c64f46554
kwuh91 <nkt.ochkin@gmail.com>
2023-03-11 02:06:59 +0300

    HW01fixed

--- lesson_1/Task5.py
+++ lesson_1/Task5.py
@@ -1,5 +1,6 @@
 # line, numb = input(), int(input())
 # print(''.join([chr((ord(i) + numb) - 26) if ord(i) + numb > ord('z') else chr((ord(i) + numb)) for i in line.replace(' ', '')]))
+
 arr, line, numb, ans = [], input(), int(input()), ""
 for i in range(97, 123):
     arr.append(chr(i))
