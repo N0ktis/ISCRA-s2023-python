bbde8b3de62d1ab84072085742f527c28d51d829
Gleb Karpikhin <glebkarpikhin@gmail.com>
2023-03-12 01:27:52 +0300

    refactored 5task

--- lesson_1/home_tasks/5task.py
+++ lesson_1/home_tasks/5task.py
@@ -2,18 +2,5 @@ a = input().lower()
 shift = int(input())
 newStr = ''
 for i in a:
-    if ord(i) + shift <= 122:
-        newStr += chr(ord(i) + shift)
-    else:
-        newStr += chr(96 + (shift - (ord('z') - ord(i))))
-
+    newStr += chr(97 + ((ord(i) - 97) + shift) % 26)
 print(newStr)
-
-a = ''
-for i in newStr:
-    if ord(i) - shift >= 97:
-        a += chr(ord(i) - shift)
-    else:
-        a += chr(123 - (shift - (ord(i) - ord('a'))))
-a = a.replace(':', ' ')
-print(f'decoded \n{a}')
