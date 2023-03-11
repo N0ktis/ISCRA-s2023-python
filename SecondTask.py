084b47a05bd2ae4db2052af19f35964e44ae4024
illegalGoose <112951442+illegalGoose@users.noreply.github.com>
2023-03-11 13:40:19 +0300

    Update SecondTask.py

--- SecondTask.py
+++ SecondTask.py
@@ -1,7 +1,7 @@
 string = input()
 while True:
     rules = input()
-    if rules[0] in string:
+    if rules != 'end':
         string = string.replace(rules[0], rules[3])
         print (string)
     else:
