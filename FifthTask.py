93962299857f2ebc8204264989f47f8682be4fcd
illegalGoose <112951442+illegalGoose@users.noreply.github.com>
2023-03-13 01:48:40 +0300

    Update FifthTask.py

--- FifthTask.py
+++ FifthTask.py
@@ -1,18 +1,6 @@
 string = input()
 key = int(input())
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
 result = ''
-i = 0
-while i < len(string):
-    if string[i] != ' ':
-        element = alphabet.find(string[i]) + key
-        if element < len(alphabet):
-            result = result + alphabet[element]
-        else:
-            while element >= len(alphabet):
-                element = element - len(alphabet)
-            result = result + alphabet[element - 4]
-    else:
-        result = result + 'u'
-    i = i + 1
-print (result)
+for letter in string:
+    result = result + chr(((ord(letter) + key - 97) % 26) + 97)
+print(result)
