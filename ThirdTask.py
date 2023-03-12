cd09116a268ab6966ad529f5001fc5ea911fe092
illegalGoose <112951442+illegalGoose@users.noreply.github.com>
2023-03-12 16:08:44 +0300

    Update ThirdTask.py

--- ThirdTask.py
+++ ThirdTask.py
@@ -1,15 +1,1 @@
-string = input()
-array = string.split()
-newString = ''
-for el in array:
-    if el.find('.') != -1:
-        if el.find('-') == 0:
-            newString = newString + el[1:el.find('.')] + ' '
-        else:
-            newString = newString + el[0] + ' '
-    else:
-        if el.find('-') == 0:
-            newString = newString + el[1:] + ' '
-        else:
-            newString = newString + el + ' '
-print (newString)
+print([int((float(num) ** 2) ** 0.5) for num in input().split()])
