ec5c11f20fda66360b01cc58d4a877f4f3c56366
Matvey Zheltikov <l-a-03@inbox.ru>
2023-03-13 21:09:01 +0300

    fix

--- homework/hm1/main4.py
+++ homework/hm1/main4.py
@@ -1,8 +1,10 @@
 #problem5
-str = input()
+str = input().replace(' ', '')
 key = int(input())
 result = ''
 for iter in str:
-    iter = chr(ord(iter) + key)
+    if ord(iter) < 97:
+        iter = chr(ord(iter) + 32)
+    iter = chr(( (ord(iter) + key) - 123) % 26 + 97)
     result+=iter
 print(result)
