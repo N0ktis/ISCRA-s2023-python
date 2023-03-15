5af8ba7e8bbd71a5e9eda8ec1f18056f43b09616
Roman Istomin <istomingg@gmail.com>
2023-03-15 12:28:11 +0300

    new code

--- py2.py
+++ py2.py
@@ -2,10 +2,8 @@ string = input()
 while True:
     replacement = input()
     split_string = replacement.split('->')
-
-    for words in split_string:
-        word = split_string[0]
-        num = split_string[1]
+    word = split_string[0]
+    num = split_string[1]
     replacement_string = string.replace(word, num)
     print(replacement_string)
     string = replacement_string
