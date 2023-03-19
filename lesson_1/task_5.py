eb4b55e948c5b53b49a3ec88423e681aed1ac0f9
PrincesaPypka <nikolya.2002@list.ru>
2023-03-19 19:28:17 +0300

    bugfix task 5

--- lesson_1/task_5.py
+++ lesson_1/task_5.py
@@ -1,9 +1,10 @@
 def main():
-    string = input().replace(' ', '')
+    string = input()
     shift = int(input())
-    print("".join(chr((ord('a') + ((ord(char.lower()) + shift) % (ord('z') + 1)) % ord('a')) % 127) for char in string))
+    print("".join(
+        chr(ord('a') + (ord(char.lower()) - ord('a') + shift) % (ord('z') - ord('a') + 1)) for char in string
+    ))
 
 
 if __name__ == "__main__":
     main()
-
