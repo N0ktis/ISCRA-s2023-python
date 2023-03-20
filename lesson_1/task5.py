c2fbaafa00d29440987e59cd6f13bec80214ae88
Ann Isaeva <isaevaan6@yandex.ru>
2023-03-20 20:41:52 +0300

    changes in task5.py

--- lesson_1/task5.py
+++ lesson_1/task5.py
@@ -1,9 +1,12 @@
 def main():
-    main_line = input()
+    main_line = input().lower().replace(" ", "")
     key = int(input())
     answer = []
     for i in main_line:
-        answer.append((chr((ord(i) + key))))
+        if ord(i) + key > 122:
+            answer.append(chr(((ord(i) + key))%122 + 96))
+        else:
+            answer.append(chr((ord(i) + key)))
     for i in answer:
         print(i, end = "")
 if __name__== '__main__':
