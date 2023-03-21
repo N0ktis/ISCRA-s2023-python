94b28ccd70c343194a16cf4103602bd44a07588f
PrincesaPypka <nikolya.2002@list.ru>
2023-03-21 19:10:18 +0300

    initial file for decompression algorythm added

---
+++ lesson_2/lzw_decompression.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+
+def main(input_filename: str, output_filename: str) -> None:
+    return None
+
+
+if __name__ == "__main__":
+    if not (2 <= len(argv) <= 3):
+        print("Error: Wrong number of arguments.")
+        print("Usage: python", argv[0], "input_file (output_file)")
+        exit(-1)
+    else:
+        main(argv[1], argv[2] if len(argv) == 3 else argv[1] + ".lzw")
