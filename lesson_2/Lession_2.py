120f36f2c8a560a739f6174e14aa8640eec74c5e
gkozhemiakin <kga.2004@mail.ru>
2023-04-03 21:27:34 +0300

    Исправил ошибки

--- lesson_2/Lession_2.py
+++ lesson_2/Lession_2.py
@@ -2,15 +2,14 @@ import math
 import sys
 
 if not (2 <= len(sys.argv) <= 3):
-    print('Usage: python', sys.argv[0], 'input_file (output_file)')
+    print('Usage: python', sys.argv[0], 'input_file')
     exit(-1)
 
-with open(sys.argv[1], 'rb') as file:
+with open(sys.argv[1],'rb') as file:
     data = file.read()
 
 outpath = sys.argv[2] if len(sys.argv) == 3 else sys.argv[1] + '.txt'
-
-dictionary = {i: (i, ) for i in range(256)}
+dictionary = {i: (i,) for i in range(256)}
 sequence = list()
 i = 0
 
@@ -20,22 +19,18 @@ with open(outpath, 'wb') as file:
             n = len(dictionary)
         else:
             n = len(dictionary) + 1
-
         nbits = math.ceil(math.log2(n))
-        if n % 8 != 0:
-            nbytes = nbits // 8 + 1
-        else:
+        if nbits % 8 == 0:
             nbytes = nbits // 8
-
-        Slice = data[i:i+nbytes]
-        sym = int.from_bytes(Slice, 'little')
-
-        if sym == len(dictionary):
-            new_sequence = sequence + sequence[-1]
         else:
-            new_sequence = dictionary[sym]
-
-        file.write(new_sequence)
-        dictionary[len(dictionary)] = sequence + new_sequence[-1]
+            nbytes = nbits // 8 + 1
+        Slice = data[i:i + nbytes]
+        index = int.from_bytes(Slice, 'little')
+        if index == len(dictionary):
+            new_sequence = sequence + sequence[-1:]
+        else:
+            new_sequence = list(dictionary[index])
+        file.write(bytearray(new_sequence))
+        dictionary[len(dictionary)] = tuple(sequence + new_sequence[-1:])
         i += nbytes
         sequence = new_sequence
